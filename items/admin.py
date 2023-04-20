1315f48aa5ae3933ec3ebaa20fcecf58694e6ff6
nddpj00 <danieljones625@btinternet.com>
2023-04-20 19:51:43 +0000

    adds items to database

---
+++ items/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Item, Category
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+# Register your models here.
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ items/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Item(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    manufacturer = models.CharField(max_length=254, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
