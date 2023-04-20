25ac76181143e8635e85f68cedd26b3de4aa5e86
nddpj00 <danieljones625@btinternet.com>
2023-04-20 20:29:36 +0000

    added items views and templates

--- items/views.py
+++ items/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . models import Item
 
-# Create your views here.
+def all_items(request):
+    """ A view to show all items, including sorting and search queries """
+
+    items = Item.objects.all()
+
+    context = {
+        'items' : items,
+    }
+
+    return render(request, 'items/items.html', context)
