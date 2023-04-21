e1c19d078488bc7fb87394a89711e61e0c579aab
nddpj00 <danieljones625@btinternet.com>
2023-04-21 09:20:16 +0000

    added item details

--- items/urls.py
+++ items/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_items, name='items')
+    path('', views.all_items, name='items'),
+    path('<item_id>', views.item_detail, name='item_detail'),
 ]
--- items/views.py
+++ items/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from . models import Item
+from django.shortcuts import render, get_object_or_404
+from . models import Item, Category
+
 
 def all_items(request):
     """ A view to show all items, including sorting and search queries """
@@ -11,3 +12,16 @@ def all_items(request):
     }
 
     return render(request, 'items/items.html', context)
+
+
+
+def item_detail(request, item_id):
+    """ A view to show all item"""
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'items/item_detail.html', context)
