7b5642c409306c46ec0aa46ea7b5720a926edf4c
nddpj00 <danieljones625@btinternet.com>
2023-04-28 10:35:16 +0000

    add bag_contens function that includes the contexts. contexts added to settings

--- deal_toolshare/settings.py
+++ deal_toolshare/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'bag.contexts.bag_contents',
             ],
         },
     },
