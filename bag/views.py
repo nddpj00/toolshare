a0a799a84549d4378579b3e7d047b28af6856087
nddpj00 <danieljones625@btinternet.com>
2023-05-02 11:54:23 +0000

    adds toasts and success message

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from items.models import Item
+
 
 def view_bag(request):
     """ A view that renders the bag contents page """
@@ -8,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    item = Item.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -16,6 +21,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'{item.name} dropped into your tool bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
