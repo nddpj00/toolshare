4db7b075e42478465a8c70110faf9fc1dc16a25a
nddpj00 <danieljones625@btinternet.com>
2023-05-05 12:38:43 +0000

    added stipe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MxpeKBHuLIEiMgOyFwTYXCSZoHS5wlOunugjhot6RvnoVeY9C3RRHoT1MnQpYVfTEK0LqDGtcn1CRKbvRsy0dUj00j8MW3R7M',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
