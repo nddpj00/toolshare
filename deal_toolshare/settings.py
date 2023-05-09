e700dcb540c406413993b1e1b0508d088a6648be
nddpj00 <danieljones625@btinternet.com>
2023-05-09 09:55:19 +0000

    updated checkout

--- deal_toolshare/settings.py
+++ deal_toolshare/settings.py
@@ -171,7 +171,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Stripe
-STRIPE_CURRENCY = 'gbp'
+STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY  = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY  = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
