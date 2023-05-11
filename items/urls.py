b954e53e2c724e083f50355585e998458859543c
nddpj00 <danieljones625@btinternet.com>
2023-05-11 18:27:02 +0000

    adds edit item facility

--- items/urls.py
+++ items/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_items, name='items'),
     path('<int:item_id>/', views.item_detail, name='item_detail'),
     path('add/', views.add_item, name='add_item'),
+    path('edit/<int:item_id>/', views.edit_item, name='edit_item'),
 ]
--- items/views.py
+++ items/views.py
@@ -62,7 +62,7 @@ def all_items(request):
 
 
 def item_detail(request, item_id):
-    """ A view to show individual product details """
+    """ A view to show individual item details """
 
     item = get_object_or_404(Item, pk=item_id)
 
@@ -75,7 +75,7 @@ def item_detail(request, item_id):
 
 
 def add_item(request):
-    """ Add a product to the store """
+    """ Add a item to the store """
     if request.method == 'POST':
         form = ItemForm(request.POST, request.FILES)
         if form.is_valid():
@@ -90,6 +90,29 @@ def add_item(request):
     template = 'items/add_item.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+def edit_item(request, item_id):
+    """ Edit an item  """
+    item = get_object_or_404(Item, pk=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated item!')
+            return redirect(reverse('item_detail', args=[item.id]))
+        else:
+            messages.error(request, 'Failed to update item. Please ensure the form is valid.')
+    else:
+        form = ItemForm(instance=item)
+        messages.info(request, f'You are editing {item.name}')
+
+    template = 'items/edit_item.html'
+    context = {
+        'form': form,
+        'item': item,
     }
 
     return render(request, template, context)
