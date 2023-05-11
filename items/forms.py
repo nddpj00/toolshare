4e8125d7e81bd789ae3ad59a9ed10775c7536c8b
nddpj00 <danieljones625@btinternet.com>
2023-05-11 19:34:27 +0000

    fixing image field on admin edit or add item

--- items/forms.py
+++ items/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Item, Category
 
 
@@ -7,6 +8,8 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
