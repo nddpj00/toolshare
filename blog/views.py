e34fdd0d4ef2497e6f3e6531c4a17cb739e0caff
nddpj00 <danieljones625@btinternet.com>
2023-05-15 12:15:26 +0000

    adds to home page view to include latest blogs

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Article
 
 def article_list(request):
-    articles = Article.objects.all().order_by('date')
+    articles = Article.objects.all().order_by('-date')
 
     template = 'blog/article_list.html'
     context = {
--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from blog.models import Article
 
-def index(request):
-    """ A view to return the index page """
-    return render(request, 'home/index.html')
+def index(request,):
+    """ A view to return the index page and add 4 most recent blogs"""
+    recentBlogs = Article.objects.exclude(thumb__isnull=True).order_by('-date')[:6]
+
+    context = {
+        'articles' : recentBlogs,
+    }
+
+    return render(request, 'home/index.html', context)
