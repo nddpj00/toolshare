fcfca7de54e9a88d3e1bb24d087e30a30bfa4171
nddpj00 <danieljones625@btinternet.com>
2023-05-20 09:50:54 +0000

    adds view to index 'home' page to render most recent events

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from blog.models import Article
+from events.models import Event
 
 def index(request,):
-    """ A view to return the index page and add 4 most recent blogs"""
+    """ A view to return the index page and add most recent blogs and events"""
     recentBlogs = Article.objects.exclude(thumb__exact='').order_by('-date')[:6]
+    recentEvents = Event.objects.exclude(thumb__exact='').order_by('-date')[:6]
 
     context = {
         'articles' : recentBlogs,
+        'events' : recentEvents,
     }
 
     return render(request, 'home/index.html', context)
