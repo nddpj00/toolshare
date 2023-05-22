0186c0aee107cdcdf9403d0c5a5532e45cf11caa
nddpj00 <danieljones625@btinternet.com>
2023-05-22 23:21:23 +0000

    correct proc typo

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn boutique_ado.wsgi:application
+web: gunicorn deal_toolshare.wsgi:application
--- deal_toolshare/settings.py
+++ deal_toolshare/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-swe@dqi5@q_+-ov)ezl!3-*@--j$l*cy0w(b-znma1s&z*je=g'
+SECRET_KEY = '0mpmpu-vwt5fkdrjp9*of$zqr$8qdy^i2%b_zxnifc^09uq)fa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
