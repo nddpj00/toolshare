8e62c1382ad664e3f17121928fc5bcb340b21421
nddpj00 <danieljones625@btinternet.com>
2023-05-22 23:25:53 +0000

    correct proc typo and set new secret key

--- deal_toolshare/settings.py
+++ deal_toolshare/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0mpmpu-vwt5fkdrjp9*of$zqr$8qdy^i2%b_zxnifc^09uq)fa'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['share-bear-toolshare.herokuapp.com', 'localhost']
 
