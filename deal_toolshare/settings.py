8fca2473a6a5037aaa3c3248840aeefe1bce9525
nddpj00 <danieljones625@btinternet.com>
2023-05-25 10:52:12 +0000

    set up emai with django - switching of development

--- deal_toolshare/settings.py
+++ deal_toolshare/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['share-bear-toolshare.herokuapp.com', 'localhost']
 
