e96d716b91680db03f026077d45e12740a507d9c
nddpj00 <danieljones625@btinternet.com>
2023-05-28 19:18:52 +0000

    styling and adding to crispy form.

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,23 +1,28 @@
+from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field
-from django import forms
+from crispy_forms.bootstrap import PrependedText, StrictButton
 from newsletter.models import Newsletter
 
 class NewsletterSubscriptionForm(forms.ModelForm):
-    agree_to_terms = forms.BooleanField(label='I agree to the data terms', required=True)
+    agree_terms = forms.BooleanField(label='I agree to the data terms', required=True)
 
     class Meta:
         model = Newsletter
-        fields = ['first_name', 'email', 'agree_to_terms']
+        fields = ['first_name', 'email', 'agree_terms']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'placeholder': 'First Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+        }
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter your first name'
-        self.fields['email'].widget.attrs['placeholder'] = 'Enter your email'
+        super(NewsletterSubscriptionForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_show_labels = False
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'd-none'
         self.helper.layout = Layout(
-            Field('first_name'),
-            Field('email'),
-            Field('agree_to_terms', wrapper_class='checkbox-field')
+            PrependedText('first_name', '<i class="fa fa-user"></i>', placeholder='First Name'),
+            PrependedText('email', '<i class="fa fa-envelope"></i>', placeholder='Email'),
+            'agree_terms',
+            StrictButton('Submit', type='submit', css_class='btn-primary')
         )
