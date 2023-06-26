22bfcec8a6be49a10fbfbeb46f6a2c4366004e55
nddpj00 <danieljones625@btinternet.com>
2023-06-26 10:06:15 +0000

    adds unit rest for blog app

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Article
+from .forms import ArticleForm
 
-# Create your tests here.
+
+class BlogTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.article = Article.objects.create(
+            title='Test Article',
+            slug='test-article',
+            body='This is a test article.',
+            author=self.user
+        )
+
+    # check article_list returns successfully., using the right template
+    def test_article_list_view(self):
+        response = self.client.get(reverse('blog'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/article_list.html')
+
+    
--- deal_toolshare/settings.py
+++ deal_toolshare/settings.py
@@ -141,16 +141,16 @@ WSGI_APPLICATION = 'deal_toolshare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# if 'DATABASE_URL' in os.environ:
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
 }
 
 
